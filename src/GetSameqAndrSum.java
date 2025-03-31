cd8039c82169d0561450f1b7676ad56fba0b260b
goforit615 <bupark82@gsm.hs.kr>
2025-03-31 21:44:07 +0900

    initial commit

--- src/GetSameqAndrSum.java
+++ src/GetSameqAndrSum.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class GetSameqAndrSum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        long num = scanner.nextInt();
+        long num = scanner.nextLong();
         // pass
         scanner.close();
     }
